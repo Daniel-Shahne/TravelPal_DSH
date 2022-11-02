eaf9404998ae0174602be6745a736ba7adbb4af9
Daniel-Shahne <Daniel.shahne@outlook.com>
2022-11-02 12:11:09 +0100

    Confirm password and username taken check added to register
    RegisterWindow: Added a second passwordbox for password confirmation, and also made sure to check if username is already taken. Error message changed to work similarly to the other generateErrorMsg methods.

--- TravelPal_DSH/RegisterWindow.xaml.cs
+++ TravelPal_DSH/RegisterWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace TravelPal_DSH
         private UserManager userManager;
         private TravelManager travelManager;
         bool userNameOk;
-        bool passwordOk;
+        bool passwordEquals;
 
         BrushConverter bc = new();
         Brush? badInputColor;
@@ -43,12 +43,15 @@ namespace TravelPal_DSH
             // Initialize colors
             badInputColor = (Brush)bc.ConvertFrom("#f5b8b9");
 
+            // Set password tags to false
+            pswPassword1.Tag = false;
+            pswPassword2.Tag = false;
         }
 
         /* Changed the buttons content to "Go Back"
          * Will just close the register window and
          * create and go back to a mainwindow, while
-         * sending the current usermanager instance */
+         * sending the current um/tm instances */
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
             MainWindow mainWin = new(userManager, travelManager);
@@ -56,10 +59,28 @@ namespace TravelPal_DSH
             this.Close();
         }
 
-        /* Updates userNameOk, and UI input field color,
-         * to reflect if said input would be ok as username */
+        /* Works in two steps, each being an if/else clause.
+         * First clause checks if username is already taken
+         * and returns if so, next step is otherwise to
+         * check len >= 3 */
         private void txbUsername_TextChanged(object sender, TextChangedEventArgs e)
         {
+            if (userManager.isUsernameTaken(txbUsername.Text))
+            {
+                userNameOk = false;
+                txbUsername.Background = badInputColor;
+                
+                lblError.Content = "Username already taken!";
+                lblError.Visibility = Visibility.Visible;
+                return;
+            }
+            else
+            {
+                lblError.Content = "";
+                lblError.Visibility = Visibility.Hidden;
+            }
+
+
             if (txbUsername.Text.Length >= 3)
             {
                 userNameOk = true;
@@ -72,39 +93,68 @@ namespace TravelPal_DSH
             }
         }
 
-        /* Updates passwordOk, and UI input field color,
-         * to reflect if said input would be ok as username */
+        /* Called by both password fields to check valid password
+         * (len >= 5) and changes their tags as success bool status
+         * for each passwordbox. A second step ensures they are equal 
+         * and uses field variable passwordEquals as status bool. */
         private void pswPassword_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            if (pswPassword.Password.Length >= 5)
+            PasswordBox psw = (PasswordBox)sender;
+
+            if (psw.Password.Length >= 5)
             {
-                passwordOk = true;
-                pswPassword.Background = Brushes.MintCream;
+                psw.Tag = true;
+                psw.Background = Brushes.MintCream;
             }
             else
             {
-                passwordOk = false;
-                pswPassword.Background = badInputColor;
+                psw.Tag = false;
+                psw.Background = badInputColor;
             }
+
+            if (pswPassword1.Password.Equals(pswPassword2.Password)) passwordEquals = true;
+            else passwordEquals = false;
         }
 
         /* Creates an account if all fields are properly
          * filled in, relying on this instance bools and
-         * a null check on cmbCountry */
+         * a null check on cmbCountry. Will also return
+         * to mainwindow if creation successfull */
         private void btnCreate_Click(object sender, RoutedEventArgs e)
         {
-            if (userNameOk && passwordOk && cmbCountry.SelectedItem is not null)
+            if (userNameOk && cmbCountry.SelectedItem is not null && (bool)pswPassword1.Tag && (bool)pswPassword2.Tag && passwordEquals)
             {
                 All_Countries country = (All_Countries)cmbCountry.SelectedItem;
-                User newUser = new(txbUsername.Text, pswPassword.Password, country);
+                User newUser = new(txbUsername.Text, pswPassword1.Password, country);
                 userManager.addUser(newUser);
                 MessageBox.Show($"Created user {newUser.Name}\nWelcome to TravelPal!", "User created", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                MainWindow mainWin = new(userManager, travelManager);
+                mainWin.Show();
+                this.Close();
             }
             else
             {
-                lblError.Content = "One or more fields arent properly filled in";
-                lblError.Visibility = Visibility.Visible;
+                generateErrorMsg();
             }
+        }
+
+
+        /* Shows error message as messagebox. Unlike other
+         * generateErrorMsg this one needs to be preceded by a check
+         * if any fields are invalid as it allways shows a msgbox */
+        private void generateErrorMsg()
+        {
+            List<string> errors = new List<string>();
+
+            if (!userNameOk) errors.Add("Username must be atleast 3 characters and not taken");
+            if (!passwordEquals) errors.Add("Passwords must match");
+            if (!(bool)pswPassword1.Tag || !(bool)pswPassword2.Tag) errors.Add("Password must be atleast 5 characters long");
+            if (cmbCountry.SelectedItem is null) errors.Add("Must select a country of residence");
+
+            string errorString = string.Join("\n", errors);
+
+            MessageBox.Show(errorString, "Cant create user", MessageBoxButton.OK, MessageBoxImage.Exclamation);
         }
     }
 }
